9498712f29f528b5b4e8287a57ca3860bc560d0d
nandan-unni <asnqln@gmail.com>
2020-10-10 23:37:36 +0530

    library management system

--- 03_LMS/03_lms.go
+++ 03_LMS/03_lms.go
@@ -78,24 +78,68 @@ func main() {
 		} else if choice == 3 {
 			clear()
 			fmt.Printf("\n\t\tIssue Book\n\n")
+			findSlno := -1
+			fmt.Printf("Enter the slno : ")
+			fmt.Scanf("%d", &findSlno)
+			if libBooks[findSlno].name != "" {
+				fmt.Printf("\nBook %d\n", libBooks[findSlno].slno)
+				fmt.Printf("%s by %s\n\n", libBooks[findSlno].name, libBooks[findSlno].author)
+				fmt.Printf("\nEnter the name of the issuer : ")
+				fmt.Scanf("%s", &issBooks[noOfIssBooks].issuer)
+				issBooks[noOfIssBooks].slno = libBooks[findSlno].slno
+				issBooks[noOfIssBooks].name = libBooks[findSlno].name
+				issBooks[noOfIssBooks].author = libBooks[findSlno].author
+				noOfIssBooks++
+				libBooks[findSlno].slno = 0
+				libBooks[findSlno].name = ""
+				libBooks[findSlno].author = ""
+			} else {
+				fmt.Printf("\nBook not found !")
+			}
 		} else if choice == 4 {
 			clear()
 			fmt.Printf("\n\t\tReturn Book\n\n")
+			findSlno := -1
+			fmt.Printf("Enter the slno : ")
+			fmt.Scanf("%d", &findSlno)
+			for i := 0; i < noOfIssBooks; i++ {
+				if issBooks[i].slno == findSlno {
+					findSlno = i
+				}
+			}
+			if issBooks[findSlno].name != "" {
+				fmt.Printf("Book %d\n", issBooks[findSlno].slno)
+				fmt.Printf("%s by %s\n", issBooks[findSlno].name, libBooks[findSlno].author)
+				fmt.Printf("Issued to %s\n\n", issBooks[findSlno].issuer)
+				pause()
+				retSlNo := issBooks[findSlno].slno
+				libBooks[retSlNo].slno = issBooks[findSlno].slno
+				libBooks[retSlNo].name = issBooks[findSlno].name
+				libBooks[retSlNo].author = issBooks[findSlno].author
+				issBooks[findSlno].slno = 0
+				issBooks[findSlno].name = ""
+				issBooks[findSlno].author = ""
+				issBooks[findSlno].issuer = ""
+			}
 		} else if choice == 5 {
 			clear()
 			fmt.Printf("\n\t\tAvailable Books\n\n")
 			for i := 1; i < noOfLibBooks; i++ {
-				fmt.Printf("Book %d\n", libBooks[i].slno)
-				fmt.Printf("%s by %s\n\n", libBooks[i].name, libBooks[i].author)
+				if libBooks[i].name != "" {
+					fmt.Printf("Book %d\n", libBooks[i].slno)
+					fmt.Printf("%s by %s\n\n", libBooks[i].name, libBooks[i].author)
+				}
 			}
 			pause()
 		} else if choice == 6 {
 			clear()
 			fmt.Printf("\n\t\tIssued Books\n\n")
 			for i := 1; i < noOfIssBooks; i++ {
-				fmt.Printf("Book %d\n", issBooks[i].slno)
-				fmt.Printf("%s by %s\n", issBooks[i].name, libBooks[i].author)
-				fmt.Printf("Issued to %s\n\n", issBooks[i].issuer)
+				if issBooks[i].name != "" {
+					fmt.Printf("Book %d\n", issBooks[i].slno)
+					fmt.Printf("%s by %s\n", issBooks[i].name, libBooks[i].author)
+					fmt.Printf("Issued to %s\n\n", issBooks[i].issuer)
+				}
 			}
 			pause()
 		} else {
